9309bf82f6a685b187468ac484e3008b2981bd5d
Naveen Murugan <naveenmurugan@Naveens-MacBook-Air-M4.local>
2026-03-16 23:54:13 +0530

    style: add mascot favicon and apple-icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,10 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
+  icons: {
+    icon: '/icon.png',
+    apple: '/apple-icon.png',
+  },
 }
 
 export default function RootLayout({
